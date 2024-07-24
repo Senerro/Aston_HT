f1c757082e99ce0eb805600feba3ed1399ea77db
Senerro <shadowpi1999@gmail.com>
2024-07-24 23:32:05 +0300

    test commit 2

--- ci_cd/src/main/java/com/astonHT/ci_cd/controllers/MainController.java
+++ ci_cd/src/main/java/com/astonHT/ci_cd/controllers/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/home")
     public String homePage(Model model) {
-        System.out.println("test message for check Jenkins");
+        System.out.println("test message for check Jenkins 2");
         model.addAttribute("name", "Main page");
         return "home";
     }
