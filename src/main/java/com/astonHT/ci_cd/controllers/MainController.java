950492d3651ae5829c2435a777aa6b5ce6e3c6da
Senerro <shadowpi1999@gmail.com>
2024-07-24 23:24:45 +0300

    test commit

---
+++ src/main/java/com/astonHT/ci_cd/controllers/MainController.java
@@ -0,0 +1,21 @@
+package com.astonHT.ci_cd.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class MainController {
+    @GetMapping("/home")
+    public String homePage(Model model) {
+        System.out.println("test message for check Jenkins");
+        model.addAttribute("name", "Main page");
+        return "home";
+    }
+
+    @GetMapping("/")
+    public String homePageSecond(Model model) {
+        model.addAttribute("name", "Main page 2");
+        return "home2";
+    }
+}
